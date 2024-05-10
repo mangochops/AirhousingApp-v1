b3181a93329ec69ce6522803d1442c959c2ccf02
mangochops <133221712+mangochops@users.noreply.github.com>
2024-05-10 14:33:52 +0300

    third commit

--- Backend/Connection.js
+++ Backend/Connection.js
@@ -3,3 +3,6 @@ const express = require('express')
 //init app & middleware
 const app = express()
 
+app.listen(3000, () => {
+  console.log('App listening on port 3000')
+})
--- Frontend/Pages/Loggedin/Tenant/TenantHomescreen.js
+++ Frontend/Pages/Loggedin/Tenant/TenantHomescreen.js
@@ -1,3 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { ScrollView } from 'react-native-web';
+import { EvilIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
+export default function TenantHomescreen(){
+  <ScrollView styles={styles.container}>
+      <View styles={styles.header}>
+        <View>
+          <Image 
+            styles={styles.avatar}
+            source={require('/Frontend/Images/Avatar.jpeg')}
+          />
+          <View>
+            <Text>Good Evening</Text>
+            <Text>Chris Kamau</Text>
+          </View>
+          <EvilIcons name="search" size={24} color="black" />
+          <Ionicons name="notifications" size={24} color="black" />
+
+        </View>
+
+      </View>
+      <View styles={styles.adssection}>
+        
+
+      </View>
+
+  </ScrollView>
+}
+
+const styles = StyleSheet.create({
+  container:{
+
+  }
+})
